5cad05ad758b8c02dbe8e80b7095465534ba3b34
klltx <kostya.svetashov.01@gmail.com>
2023-10-03 05:48:17 +0400

    add missing items removing

--- scripts/renderUnavailableItems.js
+++ scripts/renderUnavailableItems.js
@@ -1,10 +1,25 @@
+const initUnavailableItemsListeners = (item, unavailableItems) => {
+  const trashCanButton = document.querySelector(`#trash-can-${item.id}`);
+  const itemCard = document.querySelector(`#item-card-${item.id}`);
+
+  trashCanButton.addEventListener("click", (event) => {
+    itemCard.remove();
+
+    event.preventDefault();
+  });
+};
+
 const renderUnavailableItems = (unavailableItems) => {
   const unavailableItemsContainer = document.querySelector(
     "#unavailableItemsContainer"
   );
 
   unavailableItems.forEach((item) => {
-    unavailableItemsContainer.innerHTML += `
+    const div = document.createElement("div");
+    div.className = "main__cart__unavailable-items__item";
+    div.id = `item-card-${item.id}`;
+
+    div.innerHTML += `
       <div class="main__cart__unavailable-items__item" id="item-card-${
         item.id
       }">
@@ -14,9 +29,10 @@ const renderUnavailableItems = (unavailableItems) => {
             }" alt="item photo" class="main__cart__unavailable-items__item__photo">
           
           <div class="main__cart__unavailable-items__item__info">
-            <span class="main__cart__unavailable-items__item__info__title gray-text">${
-              item.title
-            }</span>
+            <span class="main__cart__unavailable-items__item__info__title gray-text">${cutItemTitle(
+              item.title,
+              44
+            )}</span>
           
             <div class="main__cart__unavailable-items__item__info__params">
               ${
@@ -39,10 +55,13 @@ const renderUnavailableItems = (unavailableItems) => {
           <button>
             <img class="icon favorite-icon" src="./assets/icons/favorite.svg" alt="add to favorite icon">
           </button>
-          <button>
+          <button id="trash-can-${item.id}">
             <img class="icon trash-can-icon" src="./assets/icons/trash-can.svg" alt="remove icon">
           </button>
         </div>
       </div>`;
+
+    unavailableItemsContainer.appendChild(div);
+    initUnavailableItemsListeners(item, unavailableItems);
   });
 };
